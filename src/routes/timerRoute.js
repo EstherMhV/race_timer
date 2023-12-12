0b417d213bb9ff8c35dba064b47f4fcc1792a818
Diece98 <esther.mehal@gmail.com>
2023-12-12 22:06:23 +0100

    fix timer

--- src/routes/timerRoute.js
+++ src/routes/timerRoute.js
@@ -2,15 +2,12 @@ const express = require('express');
 const router = express.Router();
 const timerController = require('../controllers/timerController');
 
-router
-    .route('/users/:id_user/timers')
-    .get(timerController.listAllTimers)
-    .post(timerController.createATimer)
+
 
 router
-    .route('/timers/:id_timer')
-    .get(timerController.getATimer)
-    .put(timerController.updateATimer)
-    .delete(timerController.deleteATimer)
+    .route('/:user_id/timer')
+    .post(timerController.createATimer)
+
+
 
 module.exports = router;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -12,4 +12,6 @@ router
     .route('/login')
     .post(userController.login)
 
+
+
 module.exports = router;
